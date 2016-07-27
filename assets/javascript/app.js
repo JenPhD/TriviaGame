abdaa3e98d0ce25ce153a1e7de327c888d8c8774
JenPhD <jpemberton76@gmail.com>
2016-07-27 15:30:44 -0400

    one question and one button answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,95 @@
 $(document).ready(function () {
+	
+	var questions = [
+		{
+			quest: "Which important social theorist coined the phrase 'survival of the fittest'?",
+			answers: ["Charles Darwin", "Karl Marx", "Herbert Spencer", "Sigmund Freud", "Jane Addams"],
+			correct: "Herbert Spencer",
+			currentQuestion: false
 
-	var question1 = {
+		},
+		{
+			quest: "Which sociologist used a study of suicide to show how every action, even a seemingly completely individualist act, can have social causes?",
+			answers: ["Emile Durkheim", "Karl Marx", "Max Weber", "Harriet Martineau", "Sigmund Freud"],
+			correct: "Emile Durkheim",
+			currentQuestion: false
+		}
 
-	}
-
-	var question2 = {
-
-	}
-
-var correctAnswers = 0;
-var incorrectAnswers = 0
-var unanswered = 0;
-var timer;
-
-//Show the start button on page load or reload;
+	]
+	
+	//Show the start button on page load or reload;
 	window.onload = function () {
-		$("#start").html('<button type="button" button class=btn btn-danger">Start game!</button>');
+		$("#start").html('<button type="button" button class="btn btn-danger">Start game!</button>');
 	}
 
-//On start button click, start the first timer at 30 seconds decrementing by 1 second
-	$('#start').on('click', runTimer);
-
-
+	//reset game stats;
+	function stats () {
+		var numCorrect = 0;
+		var numIncorrect = 0;
+		var unanswered = 0;
+		var timer;
+		}
+		
+		//set the timer to 40 seconds to answer questions
 	function runTimer () {
 		var seconds = 40;
 		clearInterval (timer);
 		timer = setInterval(function () {
 			$('#timer').html('Timer: ' + seconds--);
-			if (seconds === -1) {
-				clearInterval(timer);
-				alert('Time is up!');
-			}
+				if (seconds === -1) {
+					clearInterval(timer);
+				}	
 		} , 1000);
 	}
 
-	$('#start').click(function() {
+	function beginQuiz () {
+		$('#questions').empty();
+		for (i = 0; i < questions.length; i++) {
+			if(questions[i].currentQuestion === true) {
+				var $newQuiz = $('<div>')
+					.addClass = ('current col-sm-12')
+					.attr('quest-id', i)
+					.html(questions[i].quest);
+				$('#questions').append($newQuiz);			
+			} 
+			$('.current').on('click', function() {
+				firstQuestion(this.getAttribute('quest-id'))
+			})
+		}	
+	}
+	
+	//Not sure how to go back to the for loop after an answer has been clicked;
+		//$('#answers').on('click', function() {
+			//beginQuestions(this.getAttribute('quest-id'))
+			//});
+			
+		//}
+
+		function showQuestion (index) {
+			var $newQuestion = $('<div>')
+				.addClass('question col-sm-12')
+				.html(questions[0].quest);
+			$('#questions').html($newQuestion);
+
+		}
+	
+		function showAnswers (index) {
+			var $newAnswers = $('<div>')
+				.addClass('answer col-sm-12')
+				.html('<button type="button" button class="btn btn-default">' + questions[0].answers + '</button>');
+			$('#answers').html($newAnswers);
+		}
+	
+	//On start button click, hide start button, the first timer at 40 seconds decrementing by 1 second
+
+	$('#start').on('click', function () {
+		$('#start').hide();
 		runTimer();
+		stats();
+		beginQuiz();
+		showQuestion ();
+		showAnswers ();
 	});
-		
-
-
-
-
-
-
-
-
-
-
-
 
 });
+	
